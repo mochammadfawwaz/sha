0fc1669314679969f9f436b34ed1e72644ca48e7
Mochammad Fawwaz Islami <mailmcfawwaz@gmail.com>
2023-02-21 13:47:04 +0700

    add onboarding page

--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,8 +1,29 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:sha/shared/theme.dart';
+import 'package:sha/ui/pages/onboarding_page.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const OnBoardingPage(),
+          ));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
